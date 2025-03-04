20061c2e4d62bc3d8543d58d1e58d5a53a81c22c
poulphunter <>
2025-03-04 13:55:29 +0100

    simplify

--- src/App.tsx
+++ src/App.tsx
@@ -24,30 +24,28 @@ function App() {
 
 function AppLayout() {
   return (
-    <>
-      <div className="flex w-full">
-        <div
-          id="convBlock"
-          className="hidden lg:block w-64 min-w-64 h-screen overflow-y-auto overflow-x-clip bg-base-200"
-        >
-          <ConversationList />
-        </div>
-        <div
-          id="mainBlock"
-          className="block w-full min-w-0 h-screen overflow-y-auto overflow-x-clip"
-        >
-          <Header />
-          <Outlet />
-        </div>
-        <div
-          id="settingBlock"
-          className="w-full hidden xl:block xl:max-w-md xl:min-w-md bg-base-200 overflow-y-auto overflow-x-clip "
-        >
-          <SettingDialog />
-        </div>
-        <button type="button" id="dropdown-close-helper" className="h-0 w-0" />
+    <div className="flex w-full">
+      <div
+        id="convBlock"
+        className="hidden lg:block w-64 min-w-64 h-screen overflow-y-auto overflow-x-clip bg-base-200"
+      >
+        <ConversationList />
       </div>
-    </>
+      <div
+        id="mainBlock"
+        className="block w-full min-w-0 h-screen overflow-y-auto overflow-x-clip"
+      >
+        <Header />
+        <Outlet />
+      </div>
+      <div
+        id="settingBlock"
+        className="w-full hidden xl:block xl:max-w-md xl:min-w-md bg-base-200 overflow-y-auto overflow-x-clip "
+      >
+        <SettingDialog />
+      </div>
+      <button type="button" id="dropdown-close-helper" className="h-0 w-0" />
+    </div>
   );
 }
 
